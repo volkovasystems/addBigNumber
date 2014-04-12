95efe02a282095945be45a22c6a9fc6d82e65720
Richeve S. Bebedor <richeve.bebedor@gmail.com>
2014-04-13 07:54:09 +0800

    Added final modifier. Added package.

--- addBigNumber.java
+++ addBigNumber.java
@@ -1,9 +1,12 @@
+package addBigNumber;
+
 import java.math.BigInteger;
+
 public class addBigNumber{
 	public static void main( String... numbers ){
 		try{
-			BigInteger number = addBigNumber( numbers );
-			System.out.print( number.toString( ) );
+			BigInteger sum = addBigNumber( numbers );
+			System.out.print( sum.toString( ) );
 		}catch( Exception exception ){
 			System.err.print( exception.getMessage( ) );
 		}
@@ -12,11 +15,11 @@ public class addBigNumber{
 	/*
 		This enables reflection to work on this method only.
 	*/
-	public static BigInteger addBigNumber( String... numbers ){
-		BigInteger number = new BigInteger( numbers[ 0 ] );
+	public static final BigInteger addBigNumber( String... numbers ){
+		BigInteger sum = new BigInteger( numbers[ 0 ] );
 		for( int index = 1; index < numbers.length; index++ ){
-			number = number.add( new BigInteger( numbers[ index ] ) );	
+			sum = sum.add( new BigInteger( numbers[ index ] ) );	
 		}
-		return number;
+		return sum;
 	}
 }
